ab0f8644e4f65b26420ca770a820bbe4bad04aec
Lsong <song940@gmail.com>
2023-12-25 22:32:17 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/lsongdev/fileinfo-go
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/lsongdev/fileinfo-go/fileinfo"
+)
+
+func main() {
+	filename := os.Args[1]
+	out := fileinfo.Parse(filename)
+	fmt.Println("Title:", out.Title)
+	fmt.Println("Year:", out.Year)
+	fmt.Println("Source:", out.Source)
+	fmt.Println("Resolution:", out.Resolution)
+	fmt.Println("VideoCodec:", out.VideoCodec)
+	fmt.Println("AudioCodec:", out.AudioCodec)
+	fmt.Println("Studio:", out.Studio)
+	fmt.Println("Channel:", out.Channel)
+	fmt.Println("Type:", out.Type)
+}
